47939d473722137985cdbed6f4efaeca828bd043
André Monteiro <andremonteiro-@hotmail.com>
2017-11-11 18:33:03 +0000

    Init Model User

---
+++ Web App/BonfireWebApp/BonfireWebApp/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BonfireWebApp.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        [Required]
+        [StringLength(64)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(64)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(64)]
+        public string Password { get; set; }
+
+        [Required]
+        public bool Privilege { get; set; }
+    }
+}
