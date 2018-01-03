4d780ec47a29df079c6d5277e15e303e00bd452f
Spice <jdsasm@hotmail.com>
2018-01-03 15:53:12 +0000

    VIdeoPlayer

---
+++ Mobile App/BonfireMobileApp/BonfireMobileApp/BonfireMobileApp/DescriptionPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using BonfireMobileApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BonfireMobileApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DescriptionPage : ContentPage
+    {
+        public DescriptionPage()
+        {
+            InitializeComponent();
+
+            VideoPlayer vp = new VideoPlayer("https://www.youtube.com/watch?v=3ZCnhBMdm_Q");
+            Content = vp;
+        }
+    }
+}
