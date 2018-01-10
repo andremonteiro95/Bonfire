fc8a3c9de1c4c2a3f0ea0fdcfe3444874ab89bef
André Monteiro <andremonteiro-@hotmail.com>
2018-01-10 12:13:46 +0000

    WCF Soap to WCF Rest

--- Web Service/BonfireWebService/BonfireWebService/Entities/Content.cs
+++ Web Service/BonfireWebService/BonfireWebService/Entities/Content.cs
@@ -1,22 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 
 namespace BonfireWebService.Entities
 {
+    [DataContract (Namespace ="")]
     public class Content
     {
+        [DataMember]
         public string id { get; set; }
 
+        [DataMember]
         public string Title { get; set; }
 
+        [DataMember]
         public string Description { get; set; }
 
+        [DataMember]
         public string Url { get; set; }
 
+        [DataMember]
         public string StartDate { get; set; }
 
+        [DataMember]
         public string EndDate { get; set; }
     }
 }
